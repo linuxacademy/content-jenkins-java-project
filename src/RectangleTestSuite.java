ac9f4674a9c3ae2cf6a9fc2cad8a34c9941a3f3a
Sreekanth Challa <s_challa@apple.com>
2018-06-01 19:08:41 +0530

    Added Second Test Class and Test Suite to execute all test Classes

---
+++ src/RectangleTestSuite.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+    
+   @RunWith(Suite.class)
+   @SuiteClasses( { RectangleTest.class,RectangleTestSecond.class } )
+   
+public class RectangleTestSuite {
+   
+}
